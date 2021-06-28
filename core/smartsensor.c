ab332c28a9481822066286db90d33a9cb3c5245c
ubuntuVM <wzheng@omega.com>
2021-06-28 11:57:27 -0700

    added i2c registers for sensor names

--- core/smartsensor.c
+++ core/smartsensor.c
@@ -73,6 +73,7 @@ int set_sensor_descriptor(sensor_t* sensor, int sensor_num, sensor_descriptor_t 
 
 int get_sensor_name(sensor_t* sensor, int sensor_num, sensor_name_t name)
 {
+	memset(name, 0, sizeof(sensor_name_t));
 	return sensor_indexed_read(sensor, SENSOR_0_NAME, sensor_num, name, sizeof(sensor_name_t));
 }
 
